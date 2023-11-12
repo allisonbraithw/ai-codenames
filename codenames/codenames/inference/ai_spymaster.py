b78a3ce5ee482572952d0eb8ce7f29c46a69fcef
Allison Braithwaite <alliroseb@gmail.com>
2023-11-12 12:51:33 -0800

    progress

--- codenames/codenames/inference/ai_spymaster.py
+++ codenames/codenames/inference/ai_spymaster.py
@@ -3,7 +3,7 @@ from time import sleep
 from dotenv import load_dotenv
 
 
-from codenames.game.board import initiate_board
+# from codenames.game.board import initiate_board
 from codenames.dependency_factory import dependency_factory as df
 
 load_dotenv()
@@ -23,23 +23,33 @@ def initialize_ai_spymaster(color: str = "red" ):
     )
     return spymaster
 
+def initialize_game_thread():
+    return client.beta.threads.create()
 
-if __name__ == "__main__":
-    board = initiate_board()
-    board_string = "\n".join([str(card) for card in board])
-    spymaster = initialize_ai_spymaster("red")
-    thread = client.beta.threads.create()
-    client.beta.threads.messages.create(thread.id, role="user", content=f"Here are the cards in the board: {board_string}. Return the first clue")
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=spymaster.id,
-    )
+def retrieve_messages(thread_id: str, run_id: str):
     while True:
         sleep(1)
-        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-        print(run.status)
-        if run.status == "completed":
+        message = client.beta.threads.messages.retrieve(thread_id=thread_id, run_id=run_id)
+        if message.status == "completed":
             break
+    return client.beta.threads.messages.list(thread_id=thread_id)
+
+# if __name__ == "__main__":
+    # board = initiate_board()
+    # board_string = "\n".join([str(card) for card in board])
+    # spymaster = initialize_ai_spymaster("red")
+    # thread = client.beta.threads.create()
+    # client.beta.threads.messages.create(thread.id, role="user", content=f"Here are the cards in the board: {board_string}. Return the first clue")
+    # run = client.beta.threads.runs.create(
+    #     thread_id=thread.id,
+    #     assistant_id=spymaster.id,
+    # )
+    # while True:
+    #     sleep(1)
+    #     run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+    #     print(run.status)
+    #     if run.status == "completed":
+    #         break
         
-    print(client.beta.threads.messages.list(thread_id=thread.id).data[0].content)
+    # print(client.beta.threads.messages.list(thread_id=thread.id).data[0].content)
     
