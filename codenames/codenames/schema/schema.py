aa9773350ec78ac7268491deb0c18e9a28e2a0a0
Allison Braithwaite <alliroseb@gmail.com>
2023-11-11 21:12:43 -0800

    workable schema

--- codenames/codenames/schema/schema.py
+++ codenames/codenames/schema/schema.py
@@ -1,26 +1,30 @@
-from graphene import ObjectType, Field, String, Boolean, Int
+from graphene import ObjectType, Field, String, Boolean, Int, List, NonNull, Enum, InputObjectType
 from graphene_federation import build_schema
-from enum import Enum
 
 # Define card type enum
 class CardType(Enum):
-    RED_AGENT = "red_agent"
-    BLUE_AGENT = "blue_agent"
-    BYSTANDER = "bystander"
-    ASSASSIN = "assassin"
+    RED_AGENT = 1
+    BLUE_AGENT = 2
+    BYSTANDER = 3
+    ASSASSIN = 4
     
 class Position(ObjectType):
+    x = Int()
+    y = Int()
+    
+class PositionInput(InputObjectType):
     x = Int()
     y = Int()
 
 class Card(ObjectType):
     word_value = String()
-    type_value = CardType()
+    type_value = Field(CardType)
     position = Field(Position)
     is_revealed = Boolean()
 
 class Query(ObjectType):
-    card = Field(Card, position=Position(required=True))
+    card = Field(Card, position=NonNull(PositionInput))
+    board = List(Card)
 
 
 schema = build_schema(query=Query)
